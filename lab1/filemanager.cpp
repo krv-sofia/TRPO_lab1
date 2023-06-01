8bd7e0d628279b9541a5a99766cb1745ae535c1a
Sofia Krivova <mail4studin@gmail.com>
2023-06-01 11:35:20 +0700

    исправлены ошибки

--- lab1/filemanager.cpp
+++ lab1/filemanager.cpp
@@ -6,9 +6,17 @@
 FileManager::FileManager(const QList<QString> paths) {
     if (paths.length() > 0) {
         for (auto i = paths.constBegin(); i != paths.constEnd(); ++i) {
-             FileInfo fileInfo = *i;
-             this->_files_info.push_back(fileInfo);
-         }
+            if (!_files_info.isEmpty()) {
+                QFileInfo temp(*i);
+                for (int j = 0; j < _files_info.length(); ++j) {
+                    if (temp.absoluteFilePath() == _files_info[j]._fileName) {
+                        return;
+                    }
+                }
+            }
+            FileInfo fileInfo(*i);
+            _files_info.push_back(fileInfo);
+        }
     } else {
         FileManager();
     }
@@ -19,13 +27,13 @@ void FileManager::addFile(QString path)
     FileInfo newFileInfo(path);
     for (auto i = _files_info.constBegin(); i != _files_info.constEnd(); ++i) {
         if (i->_fileName == newFileInfo._fileName) {
-            emit fileChanged("You are already tracking this file");
+            //emit fileChanged("You are already tracking this file");
             return;
         }
     }
 
      _files_info.push_back(newFileInfo);
-     emit fileChanged("\nFile '" + newFileInfo._fileName.toStdString() + "' was added.");
+     //emit fileChanged("\nFile '" + newFileInfo._fileName.toStdString() + "' size: " + std::to_string(newFileInfo._size));
 }
 
 
@@ -37,21 +45,23 @@ void FileManager::checkFiles()
         QFileInfo fileInfo(files._fileName);
         //FileInfo temp = _files_info[i];
         if (files._doesExist && fileInfo.exists()) {
-                 if (_files_info[i]._size != fileInfo.size()) {
-                     emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was changed \nfrom " + std::to_string(files._size) + " bytes" +
-                                      + " to " + std::to_string(fileInfo.size()) + " bytes");
-                     _files_info[i]._size = fileInfo.size();
-                 }
-             } else if (files._doesExist && !fileInfo.exists()) {
-                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was deleted.");
-                 _files_info[i]._doesExist = false;
-             } else if (!files._doesExist && fileInfo.exists()){
+            if (_files_info[i]._size != fileInfo.size()) {
+                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was changed \nfrom " + std::to_string(files._size) + " bytes" +
+                                  + " to " + std::to_string(fileInfo.size()) + " bytes");
+                 _files_info[i]._size = fileInfo.size();
+            }
+         } else if (files._doesExist && !fileInfo.exists()) {
+             emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was deleted.");
+             _files_info[i]._doesExist = false;
+            } else if (!files._doesExist && fileInfo.exists()) {
                  emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was created.");
                  _files_info[i]._doesExist = true;
                  _files_info[i]._size = fileInfo.size();
-             }
+        }
+        emit fileChanged("\nFile '" + files._fileName.toStdString() + "' size: " + std::to_string(files._size));
         i++;
     }
+
 }
 
 void FileManager::printLog(std::string str)
