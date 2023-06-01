47495823e94f7867cb28a1be504d4708b84a3f56
sofia <mail4studin@gmail.com>
2023-06-01 15:38:54 +0700

    исправлены ошибки

--- lab1/filemanager.cpp
+++ lab1/filemanager.cpp
@@ -4,11 +4,19 @@
 #include <QDebug>
 
 FileManager::FileManager(const QList<QString> paths) {
-    if (paths.length() > 0) {
+    if (paths.length() > 0) {      
         for (auto i = paths.constBegin(); i != paths.constEnd(); ++i) {
-             FileInfo fileInfo = *i;
-             this->_files_info.push_back(fileInfo);
-         }
+            if (!_files_info.isEmpty()) {
+                QFileInfo temp(*i);
+                for (int j = 0; j < _files_info.length(); ++j) {
+                    if (temp.absoluteFilePath() == _files_info[j]._fileName) {
+                        return;
+                    }
+                }
+            }
+            FileInfo fileInfo(*i);
+            _files_info.push_back(fileInfo);
+        }
     } else {
         FileManager();
     }
@@ -19,13 +27,11 @@ void FileManager::addFile(QString path)
     FileInfo newFileInfo(path);
     for (auto i = _files_info.constBegin(); i != _files_info.constEnd(); ++i) {
         if (i->_fileName == newFileInfo._fileName) {
-            emit fileChanged("You are already tracking this file");
             return;
         }
     }
 
      _files_info.push_back(newFileInfo);
-     emit fileChanged("\nFile '" + newFileInfo._fileName.toStdString() + "' was added.");
 }
 
 
@@ -35,23 +41,23 @@ void FileManager::checkFiles()
     int i = 0;
     for (auto &files : _files_info) {
         QFileInfo fileInfo(files._fileName);
-        //FileInfo temp = _files_info[i];
         if (files._doesExist && fileInfo.exists()) {
-                 if (_files_info[i]._size != fileInfo.size()) {
-                     emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was changed \nfrom " + std::to_string(files._size) + " bytes" +
-                                      + " to " + std::to_string(fileInfo.size()) + " bytes");
-                     _files_info[i]._size = fileInfo.size();
-                 }
-             } else if (files._doesExist && !fileInfo.exists()) {
-                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was deleted.");
-                 _files_info[i]._doesExist = false;
-             } else if (!files._doesExist && fileInfo.exists()){
-                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was created.");
+            if (files._size != fileInfo.size()) {
+                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was changed \nfrom " + std::to_string(files._size) + " bytes" +
+                                  + " to " + std::to_string(fileInfo.size()) + " bytes");
+                 _files_info[i]._size = fileInfo.size();
+            }
+         } else if (files._doesExist && !fileInfo.exists()) {
+             emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was deleted.");
+             _files_info[i]._doesExist = false;
+            } else if (!files._doesExist && fileInfo.exists()) {
+                 emit fileChanged("\nFile '" + files._fileName.toStdString() + "' was created. \nSize: " + std::to_string(fileInfo.size()));
                  _files_info[i]._doesExist = true;
                  _files_info[i]._size = fileInfo.size();
-             }
+                }
         i++;
     }
+
 }
 
 void FileManager::printLog(std::string str)
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    //QList<QString> paths_list = {"fileName1.txt", "fileName2.txt"};
-    QList<QString> paths_list = {};
+    QList<QString> paths_list = {"fileName1.txt", "fileName1.txt"};
+    //QList<QString> paths_list = {};
     FileManager manager(paths_list);
     QObject::connect(&manager, SIGNAL(fileChanged(std::string)), &manager, SLOT(printLog(std::string)));
     manager.addFile("fileName3.txt");
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
     manager.addFile("fileName4.txt");
     manager.addFile("fileName5.txt");
 
+    //manager.checkFiles();
     while (true) {
         manager.checkFiles();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
